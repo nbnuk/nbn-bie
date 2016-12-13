5c835dbe5dbe98a7b1f0e1ff39157f0a0fec51da
Dave Martin <djtfmartin@gmail.com>
2016-12-13 09:59:50 +0000

    bumped the version of ala-bootstrap3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -48,7 +48,7 @@ grails.project.dependency.resolution = {
         compile ":cache-headers:1.1.6"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        compile ':ala-bootstrap3:1.6.1'
+        compile ':ala-bootstrap3:1.6.2'
         runtime ":ala-admin-plugin:1.2"
         runtime(":ala-auth:1.3.1") {
             excludes "servlet-api"
