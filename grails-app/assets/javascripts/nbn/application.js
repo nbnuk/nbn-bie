6b0a69cf3080d8b9241f6441bcd317579531d822
Helen Manders-Jones <realhelenmjones@gmail.com>
2022-07-18 12:52:57 +0100

    extract _records.gsp - [counting]]

--- grails-app/assets/javascripts/nbn/application.js
+++ grails-app/assets/javascripts/nbn/application.js
@@ -4,6 +4,10 @@ $.getScript(jsFileLocation+'/application-last.js');
 
 $( document ).ready(function() {
 
-    $("#search-inpage search")
+    setPreLoadedOccurrenceRecordCountValue();
 
+    function setPreLoadedOccurrenceRecordCountValue(){
+        //ALA show 0 which is misleading if there is a problem and occurrenceRecordCount is not available
+        $("span.occurrenceRecordCount:not(.loaded)").text("[counting]");
+    }
 })
