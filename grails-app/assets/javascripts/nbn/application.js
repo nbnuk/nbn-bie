86022c376236adcc26d34139deebcd1032d85af6
Helen Manders-Jones <realhelenmjones@gmail.com>
2022-07-18 13:52:06 +0100

    extract _records.gsp - links

--- grails-app/assets/javascripts/nbn/application.js
+++ grails-app/assets/javascripts/nbn/application.js
@@ -4,10 +4,19 @@ $.getScript(jsFileLocation+'/application-last.js');
 
 $( document ).ready(function() {
 
-    setPreLoadedOccurrenceRecordCountValue();
+    setOccurrenceRecordCountIsLoading();
+    changeViewOccurrenceRecordLinks();
 
-    function setPreLoadedOccurrenceRecordCountValue(){
+    function setOccurrenceRecordCountIsLoading(){
         //ALA show 0 which is misleading if there is a problem and occurrenceRecordCount is not available
         $("span.occurrenceRecordCount:not(.loaded)").text("[counting]");
+    }
+
+    function changeViewOccurrenceRecordLinks(){
+        let viewAllOccurrenceRecordsLinks =$("section#records a");
+        if (typeof MAP_CONF !== 'undefined' && viewAllOccurrenceRecordsLinks.length==2){
+            $(viewAllOccurrenceRecordsLinks[0]).attr("href", MAP_CONF.viewAllOccurrenceRecordsUrl);
+            $(viewAllOccurrenceRecordsLinks[0]).attr("href", MAP_CONF.viewAllOccurrenceRecordsAsMapUrl);
+        }
     }
 })
